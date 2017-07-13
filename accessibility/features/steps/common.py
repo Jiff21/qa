6d61f0ae55cdce16a3715b9b02cb07304793666e
Jeff <jeff@Jeffs-MacBook-Pro.local>
2017-07-12 22:42:59 -0700

    breaking, need to return json in when for then.

---
+++ accessibility/features/steps/common.py
@@ -0,0 +1,23 @@
+import os
+import json
+import re
+import sys
+from behave import *
+from environment_variables import BASE_URL, QA_FOLDER_PATH
+
+FILE_NAME = 'example'
+
+results_file = '%saccessibility/output/%s.report.json' % (
+    QA_FOLDER_PATH,
+    FILE_NAME
+)
+
+@given('we have valid json alert output')
+def step_impl(context):
+    with open(results_file, 'r') as f:
+        try:
+            context.results_json = json.load(f)
+        except Exception as e:
+            sys.stdout.write('Error: Invalid JSON in %s: %s\n' %
+                             (results_file, e))
+            assert False
