543a4d4bc232e949c0d4fed2ccac851ed95359cd
Jiff21 <jeffreypatrickcampbell@gmail.com>
2017-07-18 16:58:24 -0700

    Use BASE_URL for e2e test.

--- e2e/features/google.feature
+++ e2e/features/google.feature
@@ -2,6 +2,6 @@ Feature: Google your way to documentation
 
   @browser
   Scenario: Google
-    Given I am on "https://google.com"
+    Given I am on "index"
     When I type in "Behave Python"
     Then the results should contain "Welcome to behave!"
--- e2e/features/steps/common.py
+++ e2e/features/steps/common.py
@@ -3,8 +3,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from behave import given, when, then
+from qa.environment_variables import BASE_URL
 
 
-@given('I am on "{url}"')
-def get(context, url):
-    context.driver.get(url)
+@given('I am on "{uri}"')
+def get(context, uri):
+    context.current_url == ''
+    if uri.lower() == 'index':
+        context.current_url = BASE_URL
+    else:
+        context.current_url = BASE_URL + uri
+    context.driver.get(context.current_url)
