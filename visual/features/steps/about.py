a482da22bc8ed1996358510ad29e781517904420
Jiff21 <jeffreypatrickcampbell@gmail.com>
2017-07-18 17:54:39 -0700

    Working applitools test, but need to change it to a different spot.

--- visual/features/steps/about.py
+++ visual/features/steps/about.py
@@ -3,32 +3,40 @@ Feature: The about page's style doesn't accidentally change
 
   @browser
   Scenario: I go to the about page on a tablet it should look as expected
-    Given I start eyes at "tablet"
-    When I type in "hello world!"
-      and click a certain button
-    Then it should look a certain way with the message "Click!"
+    Given I am on "/"
+      And I start "Home to other pages test" of "example.com" at "tablet"
+    When the "Home Page" should look as expected
+      And I click the sf office image
+      And locate the header
+    Then the "Contact Page" should look as expected
         and we close eyes
+
 '''
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from behave import given, when, then
-from hamcrest import assert_that, contains_string, equal_to
 from qa.environment_variables import BASE_URL, DRIVER, SELENIUM, SL_DC
 from qa.e2e.features.browser import Browser
 import time
 
-
 # Locator Map
-SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button')
+SF_OFFICE = (By.CSS_SELECTOR, 'a.office.sfo')
+CONTACT_HEADER = (By.CSS_SELECTOR, 'p.strapline')
 
 
-@when('and click a certain button')
+@when('I click the sf office image')
 def click_a_button(context):
-    context.driver.find_element(*SUBMIT_BUTTON)
+    sf_office_icon = context.driver.find_element(*SF_OFFICE)
+    time.sleep(10)
+    sf_office_icon.click()
 
 
-@then('it should look a certain way with the message "{message}"')
-def step_then_should_transform_into(context, message):
-    context.eyes.check_window(message)
+@when('locate the header')
+def step_then_should_transform_into(context):
+    header = context.driver.find_element(*CONTACT_HEADER)
+    actions = ActionChains(context.driver)
+    actions.move_to_element(header)
+    actions.perform()
