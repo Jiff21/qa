ce267243dc48b642ae7c66320acba997cb75cb69
Jiff21 <jeffreypatrickcampbell@gmail.com>
2017-07-19 12:02:41 -0700

    Changed locust runner. Changed folder names to security and performance.

---
+++ security/zap_scanner.py
@@ -0,0 +1,6 @@
+from qa.security.scanner import Scanner
+from qa.accounts import Accounts
+
+scanner = Scanner()
+accounts = Accounts()
+scanner.run('http://' + accounts.ZAP_API_IP, accounts.ZAP_API_KEY)
