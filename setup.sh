beab2963d4fcbf1861873dc859aade40be8870b3
Jiff Campbell <inajiff@gmail.com>
2017-07-19 19:05:58 -0700

    Didn't need to be that compley with shell file.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,7 @@
 #!/bin/sh
-# This checks to see if you're using a gitignore and either creates one or
-# adds necessary file ignores to existing one.
-if [-f '.gitignore']
-then
-  echo 'gitignore exits in root adding QA ignores'
-  echo '\n' >> .gitignore
-  cat qa/.gitignore >> .gitignore
-else
-  echo 'gignore does not exist, creating it in root'
-	cat qa/.gitignore > .gitignore
-fi
 
-if [-f 'Makefile']
-then
-  echo 'Makefile exists copying QA commands into it'
-	echo '\n' >> Makefile
-	cat qa/Makefile >> Makefile
-else
-  echo 'Makefile does not exist copying QA commands into it'
-	cat qa/Makefile > Makefile
-fi
+echo 'Copying or adding qa .gitignores into the root project'
+cat qa/.gitignore >> .gitignore
+echo 'Copying or adding qa Makefile into the root project'
+cat qa/Makefile >> Makefile
+echo 'Only run this command once its not smart and will double these.'
