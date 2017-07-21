813291b03a664b20bab8c9bf0836ed3726e24a91
Jiff Campbell <inajiff@gmail.com>
2017-07-21 07:13:38 -0700

    Updated analaytics test.

--- analytics/features/console_log.feature
+++
@@ -1,5 +0,0 @@
-Feature: Home Page has correct GA Tags
-
-  Scenario: The Homepage fires an event when it loads
-    When I check logs
-    Then I should see "title" with a value of "Our Products | Google"
--- analytics/features/steps/common.py
+++ analytics/features/steps/common.py
@@ -15,6 +15,31 @@ def get(context, uri):
     current_url = BASE_URL + uri
     context.driver.get(current_url)
 
+@when('I check logs')
+def check_console_logs(context):
+    context.current_url = BASE_URL + '/'
+    context.driver.get(context.current_url)
+    context.captured_logs = ['']
+    for entry in context.driver.get_log('browser'):
+        if 'https://www.google-analytics.com/analytics_debug.js' in entry['message']:
+            context.captured_logs.append(entry['message'])
+
+@then('I should see "{ga_name}" with a value of "{ga_value}"')
+def assert_no_errors_in_logs(context, ga_name, ga_value):
+    context.text_found = False
+    context.ga_search = CommonFunctions()
+    for entry in context.captured_logs:
+        if context.ga_search.find_ga_by_terms(entry, ga_name, ga_value):
+            print ('Found: %s' % entry)
+            context.text_found = True
+    try:
+        assert context.text_found is True
+    except AssertionError:
+        print ('Didn\'t find expected ga tag, found:\n')
+        for message in context.captured_logs:
+            print (str(message))
+        raise
+
 
 class CommonFunctions(object):
 
