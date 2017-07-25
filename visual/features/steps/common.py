0d18f0db7c71d67fbfe4f4964e34f1ea2516afa5
Jiff21 <jeffreypatrickcampbell@gmail.com>
2017-07-25 10:09:29 -0700

    Working on a full screen option WIP.

--- visual/features/steps/common.py
+++ visual/features/steps/common.py
@@ -1,4 +1,5 @@
 import os
+import time
 from PIL import Image, ImageDraw
 from pathlib import Path
 from pdiffer import pdiff, assert_images_similar
@@ -6,10 +7,13 @@ from behave import given, when, then
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from qa.environment_variables import BASE_URL, DRIVER, SELENIUM, SL_DC
 
 REBASE = os.getenv('REBASE', False)
+
+LOGGING_SECTION_LOCATOR = (By.ID, 'logging-setup')
 
 
 def create_diff_image(page_name, this_runs_image, difference_image):
@@ -21,6 +25,25 @@ def create_diff_image(page_name, this_runs_image, difference_image):
     overlay = overlay.convert("RGBA")
     new_img = Image.blend(background, overlay, 0.5)
     new_img.save(errors, "PNG")
+
+
+def get_element_size(element):
+    location = element.location
+    size = element.size
+    return location, size
+
+
+def crop_screenshot(image, location, size):
+    print(image)
+    original_image = Image.open(image)
+    time.sleep(2)
+    # left = location['x']
+    # top = location['y']
+    # right = location['x'] + size['width']
+    # bottom = location['y'] + size['height']
+    # cropped_image = original_image.crop((left, top, right, bottom))
+    # cropped_image.save(image, "PNG")
+    original_image.save('qa/visual/images/baselines/example.png', "PNG")
 
 
 @given('I start "{page}" at "{width}" x "{height}"')
@@ -60,38 +83,55 @@ def check_expect_given(context):
             output=context.output_path
         )
 
-#
-# @when('I create or compare a screenshot of an element')
-# def check_expect_given(context):
-#     context.should_assert = False
-#
-#     if bool(REBASE) is True:
-#         print('WARN: File does not exist, creating baseline image at qa/visual/images/baselines/%s.png' %
-#               context.current_page)
-#         # Not sure if this will work of I need to https://stackoverflow.com/questions/13832322/how-to-capture-the-screenshot-of-a-specific-element-rather-than-entire-page-usin
-#         context.current_element.save_screenshot(
-#             'qa/visual/images/baselines/%s.png' % context.current_page)
-#     else:
-#         context.should_assert = True
-#         context.current_element.save_screenshot(
-#             'qa/visual/images/current_run/%s.png' % context.current_page)
-#         context.current_baseline = 'qa/visual/images/baselines/%s.png' % context.current_page
-#         context.current_run = 'qa/visual/images/current_run/%s.png' % context.current_page
-#         # print('Compare:\n%s\nTo:\n%s' %
-#         #       (context.current_baseline, context.current_run))
-#         context.output_path = 'qa/visual/images/diff/%s.png' % context.current_page
-#         context.current_screenshot = pdiff(
-#             context.current_baseline,
-#             context.current_run,
-#             # threshold=.01,
-#             output=context.output_path
-#         )
-#
+
+@when('I find the logging setup section')
+def find_logging(context):
+    context.current_element = context.driver.find_element(
+        *LOGGING_SECTION_LOCATOR)
+
+
+@when('I create or compare a screenshot of an element')
+def check_expect_element(context):
+    context.should_assert = False
+    context.page_width = context.driver.execute_script(
+        'return document.documentElement.clientWidth')
+    context.page_height = context.driver.execute_script(
+        'return document.documentElement.clientHeight')
+    context.driver.set_window_size(context.page_height, context.page_width)
+    if bool(REBASE) is True:
+        print('WARN: File does not exist, creating baseline image at qa/visual/images/baselines/%s.png' %
+              context.current_page)
+        # Not sure if this will work of I need to https://stackoverflow.com/questions/13832322/how-to-capture-the-screenshot-of-a-specific-element-rather-than-entire-page-usin
+        context.driver.save_screenshot(
+            'qa/visual/images/baselines/%s.png' % context.current_page)
+        location, size = get_element_size(context.current_element)
+        print (location)
+        print (size)
+        screenshot_path = 'qa/visual/images/baselines/%s.png' % context.current_page
+        crop_screenshot(screenshot_path, location, size)
+    else:
+        context.should_assert = True
+        context.driver.save_screenshot(
+            'qa/visual/images/current_run/%s.png' % context.current_page)
+        location, size = get_element_size(context.current_element)
+        element_path = 'qa/visual/images/baselines/%s.png' % context.current_page
+        crop_screenshot(element_path, location, size)
+        context.current_baseline = 'qa/visual/images/baselines/%s.png' % context.current_page
+        context.current_run = 'qa/visual/images/current_run/%s.png' % context.current_page
+        # print('Compare:\n%s\nTo:\n%s' %
+        #       (context.current_baseline, context.current_run))
+        context.output_path = 'qa/visual/images/diff/%s.png' % context.current_page
+        context.current_screenshot = pdiff(
+            context.current_baseline,
+            context.current_run,
+            # threshold=.01,
+            output=context.output_path
+        )
 
 
 @then('it should match')
-def check_expect_when(context):
-    print(bool(context.current_screenshot))
+def match_if_assert_is_true(context):
+    # print(bool(context.current_screenshot))
     if context.should_assert is True:
         # Creating difference image
         if bool(context.current_screenshot) is True:
--- visual/features/testing.feature
+++ visual/features/testing.feature
@@ -1,8 +1,17 @@
 Feature: The about page's style doesn't accidentally change
 
   @browser
-  Scenario:
+  Scenario: Take a screenshot of the page
     Given I am on "/behave/parse_builtin_types.html"
       And I start "about" at "600" x "800"
     When I create or compare a screenshot
+    Then it should match
+
+
+  @browser
+  Scenario: Take a screenshot of an element
+    Given I am on "/behave/api.html#logging-setup"
+      And I start "logging_element" at "600" x "800"
+    When I find the logging setup section
+      And I create or compare a screenshot of an element
     Then it should match
