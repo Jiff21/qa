58993290feb20d792b31b0d32de3a42208195321
Jiff21 <jeffreypatrickcampbell@gmail.com>
2017-08-16 10:14:39 -0700

    actually we don't need login for ga here.

--- analytics/features/environment.py
+++ analytics/features/environment.py
@@ -1,61 +1,13 @@
 import os
 import sys
 from behave import *
-from qa.analytics.features.browser import Browser
-from qa.environment_variables import BASE_URL
-from qa.environment_variables import ADMIN_EMAIL, ADMIN_PASSWORD, ADMIN_NAME
-from qa.environment_variables import EDITOR_EMAIL, EDITOR_PASSWORD, EDITOR_NAME
-from qa.environment_variables import USER_EMAIL, USER_PASSWORD, USER_NAME
-from qa.functional.features.steps.login import LoginPage
-import time
-
-ACCOUNTS = {
-    'admin': {
-        'email': ADMIN_EMAIL,
-        'password': ADMIN_PASSWORD,
-        'name': ADMIN_NAME
-    },
-    'editor': {
-        'email': EDITOR_EMAIL,
-        'password': EDITOR_PASSWORD,
-        'name': EDITOR_NAME
-    },
-    'user': {
-        'email': USER_EMAIL,
-        'password': USER_PASSWORD,
-        'name': USER_NAME
-    }
-}
+from qa.functional.features.browser import Browser
 
 
-def before_all(context):
+def before_scenario(context, feature):
     context.browser = Browser()
     context.driver = context.browser.get_browser_driver()
-    context.driver.get(BASE_URL)
-    print(USER_EMAIL)
-    email = ACCOUNTS['user']['email']
-    password = ACCOUNTS['user']['password']
-    name = ACCOUNTS['user']['name']
-    login = LoginPage(context.driver)
-    login.oauth_logic(email, password, name)
-    # context.cookies = context.driver.get_cookies()
-    # for cookie in context.cookies:
-    #     if cookie['name'] == 'GCP_IAAP_AUTH_TOKEN':
-    #         context.session_cookie = cookie
-    #     elif cookie['name'] == 'GCP_IAAP_XSRF_NONCE':
-    #         context.xsrf_nonce = cookie
-    #     else:
-    #         print('these are not the droids (cookies) you\'re looking for')
 
 
-def after_all(context):
+def after_scenario(context, feature):
     context.driver.quit()
-
-#
-# def before_scenario(context, feature):
-#     context.browser = Browser()
-#     context.driver = context.browser.get_browser_driver()
-#
-#
-# def after_scenario(context, feature):
-#     context.driver.quit()
