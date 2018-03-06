4ff54951fc4b51f0fade211133e59561b571144d
Jeff <Jeff@bynd.com>
2018-03-06 10:18:56 -0800

    Seems to be running, not sure env variables will work as intended.

--- wait-for-node.sh
+++ wait-for-node.sh
@@ -6,6 +6,21 @@
 # Some variables stolen from wait-for.
 TIMEOUT=60
 QUIET=0
+SELENIUM_ADDRESS='http://localhost:4444'
+
+
+usage() {
+  exitcode="$1"
+  cat << USAGE >&2
+Usage:
+  $cmdname browsername:version [-t timeout] [-- command args]
+  -q | --quiet                        Do not output any status messages
+  -t TIMEOUT | --timeout=timeout      Timeout in seconds, zero for no timeout
+  -- COMMAND ARGS                     Execute command with args after the test finishes
+USAGE
+  exit "$exitcode"
+}
+
 
 while [ $# -gt 0 ]
 do
@@ -53,6 +68,36 @@ VERSION_REGEX=".version=$VERSION,"
 printf "BROWSER_REGEX is %s\n" "$BROWSER_REGEX"
 printf "The VERSION_REGEX is %s\n" "$VERSION_REGEX"
 
-HUB_RESPONSE=$(curl http://localhost:4444/grid/console#)
+# if [[ $HUB_RESPONSE =~ $VERSION_REGEX && $HUB_RESPONSE =~ $BROWSER_REGEX  ]]; then echo "BROWSER FOUND:\n%s" "${BASH_REMATCH[1]}"; fi
+# HUB_RESPONSE=$(curl $SELENIUM_ADDRESS/grid/console#)
+#if [[ $HUB_RESPONSE =~ $VERSION_REGEX && $HUB_RESPONSE =~ $BROWSER_REGEX  ]];
+#then
+#  echo "Browser and Version match found"
+#else
+#  echo "Did not find a match for both browser and version."
+#fi
 
-if [[ $HUB_RESPONSE =~ $BROWSER_REGEX]] && [[$HUB_RESPONSE =~ $VERSION_REGEX ]]; then echo "BROWSER FOUND:\n%s" "${BASH_REMATCH[1]}"; fi
+wait_for() {
+  for i in `seq $TIMEOUT` ; do
+    HUB_RESPONSE=$(curl $SELENIUM_ADDRESS/grid/console#)
+
+    #result=$?
+    if [[ $HUB_RESPONSE =~ $VERSION_REGEX && $HUB_RESPONSE =~ $BROWSER_REGEX  ]]; then
+      if [ $# -gt 0 ] ; then
+        exec "$@"
+      fi
+      exit 0
+    fi
+    sleep 1
+  done
+  echo "Operation timed out" >&2
+  exit 1
+}
+
+
+if [ "$BROWSER_REGEX" = "" -o "$VERSION_REGEX" = "" ]; then
+  echoerr "Error: you need to provide a Browser and Version (e.g. chrome:63.0.1)."
+  usage 2
+fi
+
+wait_for "$@"
