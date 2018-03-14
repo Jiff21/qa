1c08536e51ba53febaefbfdcfb392e472ee82924
Jeff <Jeff@bynd.com>
2018-03-14 14:56:37 -0700

    Perf runners touch _requests.csv if it doesn't exist. Remove Behave run form allure runner

--- performance/allure_runner.py
+++ performance/allure_runner.py
@@ -1,10 +1,16 @@
 import os
 import subprocess
 from qa.settings import PAGES_DICT, BASE_URL, QA_FOLDER_PATH
+from pathlib import Path
+
 
 directory = '%s/performance/results/' % QA_FOLDER_PATH
 if not os.path.exists(directory):
     os.makedirs(directory)
+
+results_csv = '%s/performance/results/_requests.csv' % QA_FOLDER_PATH
+if not os.path.isfile(results_csv):
+    Path(results_csv).touch()
 
 generated_command = 'locust --clients=50 --hatch-rate=1 --num-request=350 \
         --no-web --csv=%s/performance/results/ \
@@ -12,17 +18,6 @@ generated_command = 'locust --clients=50 --hatch-rate=1 --num-request=350 \
             QA_FOLDER_PATH,
             BASE_URL,
             QA_FOLDER_PATH)
-process = subprocess.Popen(
-    generated_command,
-    stderr=subprocess.STDOUT,
-    shell=True
-)
-process.wait()
-
-generated_command = 'BASE_URL=%s behave -f allure_behave.formatter:AllureFormatter -o  qa/utilities/allure/allure_results %s/performance/features' % (
-    BASE_URL,
-    QA_FOLDER_PATH
-    )
 process = subprocess.Popen(
     generated_command,
     stderr=subprocess.STDOUT,
--- performance/runner.py
+++ performance/runner.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
 from qa.settings import PAGES_DICT, BASE_URL, QA_FOLDER_PATH
+from pathlib import Path
 
 directory = '%s/performance/results/' % QA_FOLDER_PATH
 if not os.path.exists(directory):
     os.makedirs(directory)
+
+results_csv = '%s/performance/results/_requests.csv' % QA_FOLDER_PATH
+if not os.path.isfile(results_csv):
+    Path(results_csv).touch()
 
 generated_command = 'locust --clients=50 --hatch-rate=1 --num-request=350 \
         --no-web --csv=%s/performance/results/ \
