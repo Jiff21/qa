36c11e9984dbff0e132107a7a5769efbe575a128
Jeff <Jeff@bynd.com>
2018-03-22 11:22:26 -0700

    Updates to behave galen logic

--- visual/features/home_visual.feature
+++ visual/features/home_visual.feature
@@ -4,7 +4,33 @@ Feature: The homepage does not unexpected visual changes
   Scenario Outline: Homepage works matches galen specs on <size> <browser>
     Given we find the json for "homepage" on "<browser>" for "<size>"
       And it's valid json
-      And we get the relevant json
+      And we get sections portion of the json
+      And and get the "Main Section" section
+    When we loop get a list of objects
+      And we loop get a list of specs
+      And we make a list of errors
+    Then they should not have errors
+
+  @chrome-only
+  Examples: Homepage works matches galen specs in Chrome
+  | browser        | size            |
+  | chrome         | mobile          |
+  | chrome         | tablet          |
+  | chrome         | desktop         |
+
+  @firefox-only
+  Examples: Homepage works matches galen specs in Firefox
+  | browser        | size            |
+  | firefox        | mobile          |
+  | firefox        | tablet          |
+  | firefox        | desktop         |
+
+  @minor @wip
+  Scenario Outline: Footer and Header match galen specs on <size> <browser>
+    Given we find the json for "homepage" on "<browser>" for "<size>"
+      And it's valid json
+      And we get sections portion of the json
+      And and get the "Header and Footer" section
     When we loop get a list of objects
       And we loop get a list of specs
       And we make a list of errors
--- visual/features/steps/common.py
+++ visual/features/steps/common.py
@@ -18,16 +18,16 @@ class LoopThroughException(Exception):
         return str(self.value)
 
 
+
 @given('we find the json for "{page}" on "{browser}" for "{size}"')
 def step_impl(context, page, browser, size):
     context.browser = browser.lower()
     context.size = size.lower()
-    context.page = page.lower()
+    context.page = page.replace(" ", "-").lower()
     results_test = context.page + '-on-' + context.size + '-device-in-' + context.browser + '-browser.json'
-    sys.stderr.write('Looking for %s' % results_test)
+    print('Looking for %s' % results_test)
     context.results_file = None
     for filename in os.listdir(results_folder):
-        print(filename)
         if results_test in filename:
             context.results_file = results_folder + filename
             break
@@ -35,25 +35,43 @@ def step_impl(context, page, browser, size):
             continue
     assert context.results_file is not None, "did not find results file"
 
+
 @given('it\'s valid json')
 def step_impl(context):
     with open(context.results_file, 'r') as f:
         try:
-            context.current_json = json.load(f)
+            context.original_json = json.load(f)
         except Exception as e:
             sys.stdout.write('Error: Invalid JSON in %s: %s\n' %
                              (context.results_file, e))
             assert False
 
 
-@given('we get the relevant json')
+@given('we get sections portion of the json')
 def step_impl(context):
     try:
-        context.current_json = context.current_json['report']['nodes'][0]['nodes'][1]['nodes'][0]['sections'][0]['objects']
+        context.section_json = context.original_json['report']['nodes'][0]['nodes'][1]['nodes'][0]['sections']
     except Exception as e:
-        sys.stdout.write('Error: Could not find relevant json %s: %s\n' %
-                         (context.current_json, e))
-        assert False
+        sys.stdout.write('Error: Could not find relevant json:\n\n%s\n\n%s\n' %
+                         (context.original_json, e))
+        raise
+
+@step('and get the "{name}" section')
+def step_impl(context, name):
+    for section in context.section_json:
+        if section['name'].lower() == name.lower():
+            context.current_json = section['objects']
+            break
+        else:
+            continue
+    if not hasattr(context, 'current_json'):
+    # if context.current_json is None:
+        sys.stdout.write(
+            'Error: Could not find section json:\n\n%s\n\n' %(
+                context.section_json,
+            )
+        )
+        raise
 
 
 @step('we loop get a list of objects')
