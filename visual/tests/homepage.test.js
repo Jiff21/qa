578fc85328c13929760e668aebd32ef7257ad10d
Jeff <Jeff@bynd.com>
2018-06-05 16:08:16 -0700

    Sort of running parameterized version. Issue with tags

--- visual/tests/homepage.test.js
+++ visual/tests/homepage.test.js
@@ -1,62 +1,56 @@
 load("init.js");
 load("pages/homepage.js");
 
-testOnAllDevices("homepage test", "/", function (driver, device) {
-    new Homepage(driver).waitForIt();
-    checkLayout(driver, "qa/visual/specs/homepage.gspec", device.tags);
-});
-
-
-testOnDevice($galen.devices.desktop, "Pretend we need to write this to be a device specific CTA test", "/", function (driver, device) {
-    var homepage = new Homepage(driver).waitForIt();
-    logged("Checking color for cta button", function () {
-        checkLayout(driver, "qa/visual/specs/cta_buttons.gspec", ["usual"]);
-    })
-
-    logged("Checking color for highlighted cta button", function () {
-        homepage.hoverCTA();
-        checkLayout(driver, "qa/visual/specs/cta_buttons.gspec", ["hovered"]);
-    });
-});
+// testOnAllDevices("homepage test", "/", function (driver, device) {
+//     new Homepage(driver).waitForIt();
+//     checkLayout(driver, "qa/visual/specs/homepage.gspec", device.tags);
+// });
+//
+//
+// testOnDevice($galen.devices.desktop, "Pretend we need to write this to be a device specific CTA test", "/", function (driver, device) {
+//     var homepage = new Homepage(driver).waitForIt();
+//     logged("Checking color for cta button", function () {
+//         checkLayout(driver, "qa/visual/specs/cta_buttons.gspec", ["usual"]);
+//     })
+//
+//     logged("Checking color for highlighted cta button", function () {
+//         homepage.hoverCTA();
+//         checkLayout(driver, "qa/visual/specs/cta_buttons.gspec", ["hovered"]);
+//     });
+// });
 
 // testOnAllDevices("homepage test using " + $galen.config.browser +" browser", "/", function (driver, device) {
 //     new Homepage(driver).waitForIt();
 //     checkLayout(driver, "qa/visual/specs/homepage.gspec", device.tags);
 // });
 //
-//
-// var devices = {
-//   mobile: {
-//     deviceName: "mobile",
-//     size: "400x700"
-//   },
-//   tablet: {
-//     deviceName: "tablet",
-//     size: "600x800"
-//   },
-//   desktop: {
-//     deviceName: "desktop",
-//     size: "1024x768"
-//   }
-// };
-//
-// var browsers = {
-//   firefox: {
-//     browserName: "Firefox"
-//   },
-//   chrome: {
-//     browserName: "Chrome"
-//   }
-// };
-//
-// forAll(devices, function () {
-//     forAll(browsers, function () {
-//         test("Test on ", function (device, browser) {
-//             new Homepage(driver).waitForIt();
-//             checkLayout(driver, "qa/visual/specs/homepage.gspec", device.tags);
-//         });
+
+
+forAll($galen.devices, function () {
+    forAll($galen.browsers, function () {
+        test("Test on ${deviceName} for ${browserName}", function (device, browser) {
+            System.out.println("Testing on  "+ device.deviceName);
+            System.out.println("with a size of " + device.size + ". How to debug on command line should be in JS docs");
+            var driver = createDriver($galen.settings.website + "", device.size, browser.browserName);
+            // var driver = device.initDriver($galen.settings.website + "");
+            new Homepage(driver).waitForIt();
+            checkLayout(driver, "qa/visual/specs/homepage.gspec", browser.browserName);
+            driver.quit()
+        });
+    });
+});
+// forAll($galen.devices, function () {
+//     test("Test on ${deviceName} for ${browserName}", function (device, browser) {
+//         System.out.println("Testing on  "+ device.deviceName);
+//         System.out.println("with a size of " + device.size + ". How to debug on command line should be in JS docs");
+//         var driver = createDriver($galen.settings.website + "", device.size, browser.browserName);
+//         // var driver = device.initDriver($galen.settings.website + "");
+//         new Homepage(driver).waitForIt();
+//         checkLayout(driver, "qa/visual/specs/homepage.gspec", device.tags);
+//         driver.quit()
 //     });
 // });
-//
-//
-//
+
+
+
+
--- visual/tests/init.js
+++ visual/tests/init.js
@@ -4,6 +4,16 @@ load("galen-bootstrap/galen-bootstrap.js");
 var domain = System.getenv("BASE_URL") || "http://testapp.galenframework.com";
 // var domain = "http://testapp.galenframework.com";
 
+// $galen.settings.current_browser = System.getenv("DRIVER") || "chrome";
+// if('chrome' in current_browser){
+//     current_browser = 'chrome';
+// }else if('firefox' in current_browser){
+//     current_browser = 'firefox';
+// }else{
+//     System.out.println('Unsupported browser');
+//     System.exit(0);
+// }
+
 var TEST_USER = {
     username: "testuser@example.com",
     password: "test123"
@@ -12,33 +22,39 @@ var TEST_USER = {
 $galen.settings.website = domain;
 //$galen.settings.website = "http://localhost:8080";
 
-$galen.registerDevice("mobile", inSingleBrowser("mobile emulation", "400x1080", ["mobile"]));
-$galen.registerDevice("tablet", inSingleBrowser("tablet emulation", "640x1080", ["tablet"]));
-$galen.registerDevice("desktop", inSingleBrowser("desktop emulation", "1024x1080", ["desktop"]));
-$galen.registerDevice("large_desktop", inSingleBrowser("large desktop emulation", "1024x1080", ["large_desktop"]));
 
-// this.browsers = {
-//   firefox: {
-//     browserName: "Firefox"
-//   },
-//   chrome: {
-//     browserName: "Chrome"
-//   }
-// };
-// this.devices = {
-//   mobile: {
-//     deviceName: "mobile",
-//     size: "400x700"
-//   },
-//   tablet: {
-//     deviceName: "tablet",
-//     size: "600x800"
-//   },
-//   desktop: {
-//     deviceName: "desktop",
-//     size: "1024x768"
-//   }
-// };
+// $galen.registerDevice("mobile", inSingleBrowser("mobile emulation", "400x1080", ["mobile"]));
+// $galen.registerDevice("tablet", inSingleBrowser("tablet emulation", "640x1080", ["tablet"]));
+// $galen.registerDevice("desktop", inSingleBrowser("desktop emulation", "1024x1080", ["desktop"]));
+// $galen.registerDevice("large_desktop", inSingleBrowser("large desktop emulation", "1024x1080", ["large_desktop"]));
+
+$galen.browsers = {
+    firefox: {
+        browserName: "firefox"
+    },
+    chrome: {
+        browserName: "chrome"
+    }
+};
+
+$galen.devices = {
+    mobile: {
+          deviceName: "mobile",
+          size: "400x1000"
+     },
+    tablet: {
+          deviceName: "tablet",
+          size: "640x1080"
+     },
+    desktop: {
+          deviceName: "desktop",
+          size: "1024x1080"
+     },
+    large_desktop: {
+          deviceName: "desktop",
+          size: "1024x1080"
+     }
+};
 
 
 (function (export) {
