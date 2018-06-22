9fd18dd782fa9c404093c03808549eab2457f1ea
Jeff <Jeff@bynd.com>
2018-06-22 13:50:52 -0700

    Also send properties file

--- report_exporter.py
+++ report_exporter.py
@@ -1,15 +1,17 @@
 import os
 import requests
 from qa.settings import  ALLURE_REPORT_HUB_URL, ALLURE_PROJECT_NAME
+from qa.utilities.oauth.service_account_auth import make_iap_request
 
 
-SEND_FILE_URL = ALLURE_REPORT_HUB_URL + 'upload_file'
+SEND_FILE_URL = ALLURE_REPORT_HUB_URL + '/upload_file'
 
 # HEADERS = {
 #     "content-type":"multipart/form-data;",
 #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
 # }
 data = {'project':ALLURE_PROJECT_NAME}
+print('Project is %s' % data['project'])
 # http://docs.python-requests.org/en/latest/user/quickstart/#post-a-multipart-encoded-file
 # Send a single file example
 # files = {'file': open('test.json', 'rb')}
@@ -18,12 +20,12 @@ data = {'project':ALLURE_PROJECT_NAME}
 directory = os.path.join(os.path.abspath(os.path.dirname(__file__)),'utilities/allure/allure_results/')
 print('Send file to: %s' % SEND_FILE_URL)
 for file in os.listdir(directory):
-    if '.json' in str(file) or 'history' in str(file):
+    if '.json' in str(file) or 'history' in str(file) or 'properties' in str(file):
         print('Sending %s' % file)
         files = {'file': open(os.path.join(directory, file), 'rb')}
         r = requests.post(SEND_FILE_URL, files=files, data=data)
         r.raise_for_status()
 
 data = {'project':'example'}
-r = requests.post(ALLURE_REPORT_HUB_URL + 'build_report', data=data)
+r = requests.post(ALLURE_REPORT_HUB_URL + '/build_report', data=data)
 r.raise_for_status()
