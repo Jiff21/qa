44beb587f5e71f0d64890cb88ba0d2160d0aaecf
Jeff <Jeff@bynd.com>
2018-08-31 16:44:01 -0700

    should use mobile_ for driver if statements, safari window switch fix.

--- functional/features/steps/workarounds.py
+++ functional/features/steps/workarounds.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 def scroll_to_webelement(driver, web_element):
     '''
     actions.move_to_element will fail in firefox if you do not scroll
@@ -24,3 +25,45 @@ def scroll_to_webelement(driver, web_element):
         scroll_nav_out_of_way = 'window.scrollBy(0, -120);'
         driver.execute_script(scroll_by_coord)
         driver.execute_script(scroll_nav_out_of_way)
+
+
+def safari_window_switcher(context, title):
+    '''Safari has timing issues with which window is what.'''
+    if 'safari' in context.driver.capabilities['browserName']:
+        context.expected_title = title
+        for i in range(0, len(context.driver.window_handles)):
+            context.driver.switch_to_window(context.driver.window_handles[i])
+            if context.driver.title == context.expected_title:
+                context.handle_to_switch_to = context.driver.current_window_handle
+        print(context.driver.title)
+        context.driver.switch_to_window(context.handle_to_switch_to)
+        assert context.driver.title == context.expected_title, \
+            'Safari swindow switching issue at %s' % context.driver.current_url
+
+
+def make_sure_safari_back_on_only_window(driver):
+    '''
+        Without this it sometimes doesn't switch to the original window.
+        Oddly though the time it takes to run makes it unnecessary.
+    '''
+    if 'safari' in driver.capabilities['browserName']:
+        if len(driver.window_handles) > 1:
+            print('Waiting for only 1 tab')
+            time.sleep(0.25)
+            make_sure_safari_back_on_only_window(driver)
+        else:
+            print('Only 1 tab open!')
+            driver.switch_to_window(driver.window_handles[0])
+
+
+def safari_text_shim(selector_type, text_to_find, driver):
+    '''Safari isn't good with XPATH text selector'''
+    all_els = driver.find_elements(By.CSS_SELECTOR, selector_type)
+    for el in all_els:
+        if text_to_find in el.text:
+            print('Found text: %s' % el.text)
+            actions = ActionChains(driver)
+            actions.move_to_element(el)
+            actions.click()
+            actions.perform()
+            return
