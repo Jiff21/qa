f3e07729c4bcfa3120d57597d205beaf308ddc1b
Jiff21 <jeffreypatrickcampbell@gmail.com>
2020-02-17 04:44:35 -0800

    warn if really new python.

--- install.sh
+++ install.sh
@@ -2,40 +2,57 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT=$(dirname ${DIR})
-requiredver="3.4.0"
+requiredver="3.4"
+first_untested_python='3.8.2'
 
-if ! [ -x "$(command -v virtualenv --version)" ]; then
-  echo 'Error: virtualenv is not installed.\nPlease run pip3 install virtualenv' >&2
-  return 1
+
+
+# Python version checks
+python3ver="$(python3 --version | tr -d 'Python ')"
+if ! [ -x "$(command -v python3 --version )" ]; then
+  echo 'Python 3 not installed' >&2
+  exit 1
 fi
-
-python3ver="$(python3 --version)"
-if [! "$(printf '%s\n' "$requiredver" "$python3ver" | sort -V | head -n1)" = "$requiredver" ]; then
-  echo "System python3 greater than $requiredver. Good there."
+# Check Python is new enough
+if [ "$(printf '%s\n' "$requiredver" "$python3ver" | sort -V | head -n1)" = "$requiredver" ]; then
+  echo "User has Python $python3ver, which meets > $requiredver requirement."
 else
   echo "Less than $requiredver"
-  return 1
+  exit 1
+fi
+# Warn if Pythhon is newer than tested
+if [ "$(printf '%s\n' "$first_untested_python" "$python3ver" | sort -V | head -n1)" = "$first_untested_python" ]; then
+  printf "\n\nWARNING:Has not been tested above Python $first_untested_python may or may not work.\n\n'."
+fi
+# Check virtualenv installed
+if ! [ -x "$(command -v virtualenv --version)" ]; then
+  printf 'virtualenv is not installed.\Running pip3 install virtualenv.\n'
+  pip3 install virtualenv
 fi
 
-
+# Docker version check
 if ! [ -x "$(command -v docker --version)" ]; then
   echo 'Error: Docker is not installed.\nDonwload Docker C.E. and install from dmg file' >&2
-  return 1
+  exit 1
 fi
 
-if [ ! -d qa/env ]; then
-  virtualenv -p python3.6 qa/env
-  source qa/env/bin/activate
-else
+if [[ -d qa/env ]]; then
+  echo "virtualenv exists checking version"
   source qa/env/bin/activate
   env_python="$(python --version)"
   if [ "$(printf '%s\n' "$requiredver" "$env_python" | sort -V | head -n1)" = "$requiredver" ]; then
-    echo "Greater than or equal to $requiredver"
+    echo "Existing virtualenv using Python $env_python, meets > $requiredver requirement."
   else
     echo "Less than $requiredver"
-    return 1
+    exit 1
   fi
+else
+  echo "creating virtualenv"
+  virtualenv -p python3 qa/env
+  source qa/env/bin/activate
 fi
+
+
 
 . qa/utilities/driver_update/geckodriver.sh
 . qa/utilities/driver_update/chromedriver.sh
